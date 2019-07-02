7665f6ea21cb895e2e4501dc4a913d51e0fdfa15
Alekhya <alekhyayalla23@gmail.com>
2019-07-02 15:34:19 +0530

    Bindu, Alekhya | MOBN-510 | Add methods in service & mapper to fetch forms by list of uuids

--- omod/src/main/java/org/openmrs/module/bahmni/ie/apps/service/BahmniFormService.java
+++ omod/src/main/java/org/openmrs/module/bahmni/ie/apps/service/BahmniFormService.java
@@ -3,6 +3,7 @@ package org.openmrs.module.bahmni.ie.apps.service;
 import org.openmrs.api.OpenmrsService;
 import org.openmrs.module.bahmni.ie.apps.model.BahmniForm;
 import org.openmrs.module.bahmni.ie.apps.model.BahmniFormResource;
+import org.openmrs.module.bahmni.ie.apps.model.ExportResponse;
 
 import java.util.List;
 
@@ -11,4 +12,5 @@ public interface BahmniFormService extends OpenmrsService{
     BahmniForm publish(String formUuid);
     List<BahmniForm> getAllLatestPublishedForms(boolean includeRetired, String encounterUuid);
     List<BahmniForm> getAllForms();
+    ExportResponse getFormsByListOfUuids(List<String> formUuids);
 }
