9ac05fc5e9d34f0b7145f4bebc52c558307d0d49
Alekhya <alekhyayalla23@gmail.com>
2020-01-02 16:36:07 +0530

    Alekhya | MOBN-510 | Change the request param

--- omod/src/main/java/org/bahmni/module/bahmni/ie/apps/controller/BahmniFormController.java
+++ omod/src/main/java/org/bahmni/module/bahmni/ie/apps/controller/BahmniFormController.java
@@ -82,7 +82,7 @@ public class BahmniFormController extends BaseRestController {
 
     @RequestMapping(value = baseUrl + "/export", method = RequestMethod.GET)
     @ResponseBody
-    public ExportResponse export(@RequestParam ("uuid") List<String> uuids) {
+    public ExportResponse export(@RequestParam ("uuids") List<String> uuids) {
         return bahmniFormService.getFormsByListOfUuids(uuids);
     }
 }
